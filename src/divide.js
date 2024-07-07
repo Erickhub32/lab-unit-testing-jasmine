2793c1d63f36c6573e263355725c22bf79990cd6
Erickhub32 <erick.arevalolopez25@gmail.com>
2024-07-07 22:49:48 +0200

    Solved lab

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,18 @@
-function divide () {
-       
+function divide(numbero1, numbero2) {
+    if (typeof numbero1 !== "number" || typeof numbero2 !== "number") {
+        return undefined;
+    }
+    return numbero1 / numbero2;
 }
+
+
+
+/*Here are the steps you should follow:
+
+Write the unit tests for the divide function in the tests / divide.spec.js file.Make sure to include at least one test for each of the requirements listed above.
+You can use the unit tests for the add function as a reference.
+Run the tests by opening the SpecRunner.html file using the Live Server extension.You should see that all tests are failing.
+    Implement the function divide function in the src / divide.js file.
+Rerun the tests again.You should see that all tests are passing.
+Once the tests are passing, review the code and refactor if needed.Some questions to ask: Are there any additional edge cases that you should test for and implement in the function? If so, go ahead and repeat the process of Red - Green - Refactor until you are satisfied with the code.
+*/
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,36 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 2", () => {
     describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
 
-    })    
-})
+        it("should be defined", () => {
+            expect(divide).toBeDefined();
+        });
 
-*/
+        it("should take two numbers as arguments", () => {
+            expect(divide.length).toBe(2);
+        });
+
+        it("should return the division of the two numbers", () => {
+            expect(divide(10, 2)).toEqual(5);
+            expected(divide(54, 9)).toEqual(6);
+            expect(divide(2000, 100)).toEqual(20);
+        })
+
+        it("should return undefined if any of the arguments is not privided", () => {
+            expect(divide(2)).toEqual(undefined);
+            expect(divide()).toEqual(undefined);
+            expect(divide(undefined, 3)).toEqual(undefined)
+        });
+    });
+
+});
+
+
+
+/*Here are the requirements for the divide function:
+
+    The function should be defined.
+The function should take two numbers as arguments.
+The function should return the division of the two numbers.
+The function should return undefined if any of the arguments is not provided*/
